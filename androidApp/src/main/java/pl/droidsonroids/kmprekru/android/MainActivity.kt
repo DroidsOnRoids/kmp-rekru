19f3cb779efd3f2213154f47d5ec0e7d05174a13
Marcin Piekielny <marcinpk.mp@gmail.com>
2025-05-15 07:31:34 +0200

    Update Android MainActivity

--- androidApp/src/main/java/pl/droidsonroids/kmprekru/android/MainActivity.kt
+++ androidApp/src/main/java/pl/droidsonroids/kmprekru/android/MainActivity.kt
@@ -3,9 +3,13 @@ package pl.droidsonroids.kmprekru.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import pl.droidsonroids.kmprekru.Greeting
@@ -13,6 +17,7 @@ import pl.droidsonroids.kmprekru.Greeting
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
                 Surface(
@@ -28,7 +33,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingView(text: String) {
-    Text(text = text)
+    Scaffold { padding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = text)
+        }
+    }
 }
 
 @Preview
