561977575838ff4b8fd5ff847fb14bcd9bf18b3b
Marcin Piekielny <marcinpk.mp@gmail.com>
2025-05-15 07:51:30 +0200

    Add required dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -40,7 +40,9 @@ dependencies {
     implementation(projects.shared)
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.compose.foundation)
     implementation(libs.compose.material3)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.viewmodel.compose)
     debugImplementation(libs.compose.ui.tooling)
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,7 +2,9 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.kotlinSerialization)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.skie)
 }
 
 kotlin {
@@ -15,7 +17,7 @@ kotlin {
             }
         }
     }
-    
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -24,15 +26,37 @@ kotlin {
         it.binaries.framework {
             baseName = "shared"
             isStatic = true
+            export(libs.androidx.viewmodel)
         }
     }
 
     sourceSets {
         commonMain.dependencies {
-            //put your multiplatform dependencies here
+            api(libs.kotlinx.datetime)
+            api(libs.kotlinx.coroutines.core)
+            api(libs.androidx.viewmodel)
+            api(libs.koin.core)
+
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.ktor.core)
+            implementation(libs.ktor.content.negotiation)
+            implementation(libs.ktor.serialization.json)
+            implementation(libs.ktor.mock)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+            implementation(libs.kotlinx.coroutines.test)
+            implementation(libs.koin.test)
+            implementation(libs.turbine)
+            implementation(libs.kotest.assertions)
+        }
+        androidMain.dependencies {
+            api(libs.koin.android)
+            api(libs.kotlinx.coroutines.android)
+            implementation(libs.ktor.okhttp)
+        }
+        iosMain.dependencies {
+            implementation(libs.ktor.darwin)
         }
     }
 }
